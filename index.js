b6893741e5138415185bd6f6889247a64c27a65a
glowies <comuokta@gmail.com>
2018-11-08 19:47:11 -0500

    New Leaderboard Update
    Updated the server to work with Google accounts.

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var io = require('socket.io').listen(http);
 var fs = require('fs');
 var mongodb = require('mongodb');
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://glowies:1q4ogHrhg8I7@ds061206.mlab.com:61206/heroku_ls0g949q", function (err, mdb) {
-    db = mdb.db("heroku_ls0g949q");
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://glowies2:2Ey0xJinEc3eFbEy@ds151943.mlab.com:51943/heroku_vp8n5tzw", function (err, mdb) {
     if (err) {
         console.log('Unable to connect to MongoDB', err);
     } else {
         console.log('Connection established to MongoDB');
-        collection = db.collection('leaderboard');
+        db = mdb.db("heroku_vp8n5tzw");
+        collection = db.collection('user_collection');
         blacklistCol = db.collection('blacklist');
         collection.find({}, {limit: 5, sort: [["rank", "asc"]]}).toArray(function (err, result) {
             if (err) {
@@ -40,7 +40,7 @@ io.on('connection', function (socket) {
                 console.log('Error finding in collection', err);
             } else if (result1.length) {
                 for (var k = 0; k < result1.length; k++) {
-                    if (result1[k].name == data.name) {
+                    if (result1[k].email == data.email) {
                         includes = 1;
                     }
                 }
@@ -50,97 +50,93 @@ io.on('connection', function (socket) {
         });
 
         if (includes) {
-            socket.emit('ranks', [{'name': 'YOUR', 'score': -1, 'rank': 0},
-                {'name': 'ACCOUNT', 'score': -1, 'rank': 1},
-                {'name': 'HAS', 'score': -1, 'rank': 2},
-                {'name': 'BEEN', 'score': -1, 'rank': 3},
-                {'name': 'SUSPENDED', 'score': -1, 'rank': 4}]
-            );
+            // emit account suspended message
+            // pass
         } else {
-            collection.find({"name": data.name}).toArray(function (err, result) {
+            collection.find({"uid": data.uid}).toArray(function (err, result) {
                 if (err) {
                     console.log('Error finding in collection', err);
                 } else if (result.length) {
-                    if (result[0].score < data.score) {
-                        collection.update({name: data.name}, {name: data.name, score: data.score, id: 0});
+                    // Update user highscores if necessary.
+                    if (result[0].score < data.score || result[0].length < data.length || result[0].maxCombo < data.maxCombo) {
+                        collection.updateOne({uid: data.uid}, {$set: {
+                            uid: result[0].uid,
+                            email: result[0].email,
+                            nickname: result[0].nickname,
+                            length: Math.max(result[0].length, data.length),
+                            score: Math.max(result[0].score, data.score),
+                            maxCombo: Math.max(result[0].maxCombo, data.maxCombo),
+                            photoURL: data.photoURL
+                        }});
                     }
-                    collection.find({}, {limit: 5, sort: [["score", "descending"]]}).toArray(function (err, result) {
-                        if (err) {
-                            console.log('Error finding in collection', err);
-                        } else if (result.length) {
-                            socket.emit('ranks', result);
-                        } else {
-                            console.log('No doc in collection');
-                        }
-                    });
-                }
-                else {
+                } else {
                     console.log('No doc in collection');
                 }
+
+                emitTopFive(socket);
             });
         }
 
     });
 
     socket.on('get ranks', function () {
-        collection.find({}, {limit: 5, sort: [["score", "descending"]]}).toArray(function (err, result) {
-            if (err) {
+        emitTopFive(socket);
+    });
+
+    socket.on('insert sample ranks', function () {
+        collection.insertOne({uid: "0001", email: "derp@domain.com", nickname: "Derp", length: 0, score: 0, maxCombo: 0, photoURL: ""});
+        collection.insertOne({uid: "0010", email: "herp@domain.com", nickname: "Herp", length: 0, score: 0, maxCombo: 0, photoURL: ""});
+        collection.insertOne({uid: "0011", email: "gerp@domain.com", nickname: "Gerp", length: 0, score: 0, maxCombo: 0, photoURL: ""});
+        collection.insertOne({uid: "0100", email: "zerp@domain.com", nickname: "Zerp", length: 0, score: 0, maxCombo: 0, photoURL: ""});
+        collection.insertOne({uid: "0101", email: "berp@domain.com", nickname: "Berp", length: 0, score: 0, maxCombo: 0, photoURL: ""});
+    });
+
+    socket.on('suspend', function (data) {
+        blacklistCol.insertOne({'email': data.email});
+        console.log(data.email + " Has Been Suspended...");
+    });
+
+    socket.on('check user', function (data) {
+        collection.find({"uid": data.uid}).toArray(function (err, result) {
+            if(err){
                 console.log('Error finding in collection', err);
-            } else if (result.length) {
-                socket.emit('ranks', result);
-            } else {
-                console.log('No doc in collection');
+            }else if(result.length){
+                socket.emit('user return', {newUser: result[0].nickname == "", nickname: result[0].nickname, length: result[0].length, score: result[0].score, maxCombo: result[0].maxCombo});
+            }else{
+                collection.insertOne({
+                    uid: data.uid,
+                    email: data.email,
+                    nickname: "",
+                    length: 0,
+                    score: 0,
+                    maxCombo: 0,
+                    photoURL: data.photoURL
+                });
+                socket.emit('user return', {newUser: true});
             }
         });
     });
 
-    socket.on('reset ranks', function () {
-        console.log('Ranks reset...');
-        collection.remove({});
-        collection.insert({name: "Derp", score: -1, uid: "0001"});
-        collection.insert({name: "Herp", score: -2, uid: "0010"});
-        collection.insert({name: "Gerp", score: -3, uid: "0011"});
-        collection.insert({name: "Zerp", score: -4, uid: "0100"});
-        collection.insert({name: "Berp", score: -5, uid: "0101"});
-    });
-
-    socket.on('suspend', function (data) {
-        blacklistCol.insert({'name': data.name});
-        console.log(data.name + " Has Been Suspended...");
-    });
-
-    socket.on('check user', function (data) {
-        var includes = 0;
-        collection.find({}).toArray(function (err, result) {
-            if (err) {
+    socket.on('change display name', function (data) {
+        collection.find({"uid": data.uid}).toArray(function (err, result) {
+            if(err){
                 console.log('Error finding in collection', err);
-            } else if (result.length) {
-                for (var i = 0; i < result.length; i++) {
-                    if (result[i].name == data) {
-                        includes = 1;
-                    }
-                }
-
-                blacklistCol.find({}).toArray(function (err, result) {
-                    if (err) {
-                        console.log('Error finding in collection', err);
-                    } else if (result.length) {
-                        for (var i = 0; i < result.length; i++) {
-                            if (result[i].name == data) {
-                                includes = 1;
-                            }
-                        }
-
-                        if (!includes) {
-                            collection.insert({name: data, score: 0, id: "0"});
-                        }
-                        socket.emit('user return', !includes);
-                    } else {
-                        console.log('No doc in collection');
-                    }
-                });
-            } else {
-                console.log('No doc in collection');
+                socket.emit("nickname return", {success: false});
+            }else if(result.length && result[0].nickname == "" && data.nickname.length < 16 && /^[a-z0-9]+$/i.test(data.nickname)){
+                // Only allow nickname change if the nickname field is an empty string
+                collection.updateOne({uid: data.uid}, {$set: {
+                    uid: result[0].uid,
+                    email: result[0].email,
+                    nickname: data.nickname,
+                    length: result[0].length,
+                    score: result[0].score,
+                    maxCombo: result[0].maxCombo,
+                    photoURL: result[0].photoURL
+                }});
+                socket.emit("nickname return", {success: true, nickname: data.nickname});
+            }else{
+                console.log("UID: " + data.uid + " tried illegal username change.");
+                socket.emit("nickname return", {success: false});
             }
         });
     });
@@ -152,7 +148,7 @@ io.on('connection', function (socket) {
             } else if (result1.length) {
                 var includes = 0;
                 for (var k = 0; k < result1.length; k++) {
-                    if (result1[k].name == data) {
+                    if (result1[k].email == data) {
                         includes = 1;
                     }
                 }
@@ -163,14 +159,45 @@ io.on('connection', function (socket) {
             }
         });
     });
+});
 
-    collection.find({}, {limit: 5, sort: [["score", "descending"]]}).toArray(function (err, result) {
+function emitTopFive(socket){
+    // Initialize the leaderboardInfo object that will store information about the multiple leaderboards
+    var leaderboardInfo = Object();
+
+    // Retrieve length leaderboard
+    collection.find({}, {limit: 5, sort: [["length", "descending"]]}).toArray(function (err, result) {
         if (err) {
             console.log('Error finding in collection', err);
         } else if (result.length) {
-            socket.emit('ranks', result);
+
+            leaderboardInfo.length = result;
+
+            // Retrieve score leaderboard
+            collection.find({}, {limit: 5, sort: [["score", "descending"]]}).toArray(function (err, result) {
+                if (err) {
+                    console.log('Error finding in collection', err);
+                } else if (result.length) {
+
+                    leaderboardInfo.score = result;
+
+                    // Retrieve maxCombo leaderboard
+                    collection.find({}, {limit: 5, sort: [["maxCombo", "descending"]]}).toArray(function (err, result) {
+                        if (err) {
+                            console.log('Error finding in collection', err);
+                        } else if (result.length) {
+                            leaderboardInfo.maxCombo = result;
+                            socket.emit('ranks', leaderboardInfo);
+                        } else {
+                            console.log('No doc in collection');
+                        }
+                    });
+                } else {
+                    console.log('No doc in collection');
+                }
+            });
         } else {
             console.log('No doc in collection');
         }
     });
-});
+}
